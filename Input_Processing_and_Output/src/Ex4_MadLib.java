a3dc008701f8d90ce45b1052b78021c5d2020afb
Alessia <volpi.alessia@hotmail.it>
2024-10-24 18:12:39 +0200

    Exercise 4 - check null values in branching story values + print a more sequential branching story from the main story

--- Input_Processing_and_Output/src/Ex4_MadLib.java
+++ Input_Processing_and_Output/src/Ex4_MadLib.java
@@ -14,17 +14,15 @@ public class Ex4_MadLib {
         Scanner sc = new Scanner(in);
 
         Map<String, String> storyElements = gatherStoryElements(sc);
-        System.out.println(MessageFormat.format("Do you {0} your {1} {2} {3}? That's hilarious!",
-                storyElements.get(VERB),
-                storyElements.get(ADJECTIVE),
-                storyElements.get(NOUN),
-                storyElements.get(ADVERB)));
+        printMainStory(storyElements);
 
         Map<String, List<String>> branchingStoryElements = gatherBranchingStoryElements(sc);
 
         if(!branchingStoryElements.isEmpty()) {
             JFrame f = new JFrame();
             f.setAlwaysOnTop(true);
+            printMainStory(storyElements);
+
             while(true) {
                 String[] options = {"Random", "Alphabetically", "Reverse", "EXIT"};
                 int category = JOptionPane.showOptionDialog(f, "How would you like your story to continue?", "Let's expand the story!",
@@ -34,28 +32,32 @@ public class Ex4_MadLib {
                     break;
                 }
 
-                List<String> verbValues = branchingStoryElements.get(VERB);
-                List<String> adjectiveValues = branchingStoryElements.get(ADJECTIVE);
-                List<String> nounValues = branchingStoryElements.get(NOUN);
-                List<String> adverbValues = branchingStoryElements.get(ADVERB);
+                List<String> verbValues = branchingStoryElements.get(VERB) != null ?
+                        branchingStoryElements.get(VERB) : new ArrayList<>();
+                List<String> adjectiveValues = branchingStoryElements.get(ADJECTIVE) != null ?
+                        branchingStoryElements.get(ADJECTIVE) : new ArrayList<>();
+                List<String> nounValues = branchingStoryElements.get(NOUN) != null ?
+                        branchingStoryElements.get(NOUN) : new ArrayList<>();
+                List<String> adverbValues = branchingStoryElements.get(ADVERB) != null ?
+                        branchingStoryElements.get(ADVERB) : new ArrayList<>();
 
                 switch (category) {
                     case 0:
-                        System.out.println(MessageFormat.format("You can always {0} your {1} {2} {3}!",
+                        System.out.println(MessageFormat.format("Then you {0} your {1} {2} {3}!",
                                 verbValues.isEmpty() ? "" : verbValues.get(new Random().nextInt(verbValues.size())),
                                 adjectiveValues.isEmpty() ? "" : adjectiveValues.get(new Random().nextInt(adjectiveValues.size())),
                                 nounValues.isEmpty() ? "" : nounValues.get(new Random().nextInt(nounValues.size())),
                                 adverbValues.isEmpty() ? "" : adverbValues.get(new Random().nextInt(adverbValues.size()))));
                         break;
                     case 1:
-                        System.out.println(MessageFormat.format("You can always {0} your {1} {2} {3}!",
+                        System.out.println(MessageFormat.format("Then you {0} your {1} {2} {3}!",
                                 verbValues.isEmpty() ? "" : verbValues.getFirst(),
                                 adjectiveValues.isEmpty() ? "" : adjectiveValues.getFirst(),
                                 nounValues.isEmpty() ? "" : nounValues.getFirst(),
                                 adverbValues.isEmpty() ? "" : adverbValues.getFirst()));
                         break;
                     case 2:
-                        System.out.println(MessageFormat.format("You can always {3} your {2} {1} {0}!",
+                        System.out.println(MessageFormat.format("Then you {3} your {2} {1} {0}!",
                                 verbValues.isEmpty() ? "" : verbValues.get(new Random().nextInt(verbValues.size())),
                                 adjectiveValues.isEmpty() ? "" : adjectiveValues.get(new Random().nextInt(adjectiveValues.size())),
                                 nounValues.isEmpty() ? "" : nounValues.get(new Random().nextInt(nounValues.size())),
@@ -92,7 +94,7 @@ public class Ex4_MadLib {
         while (true) {
             List<String> values = new ArrayList<>();
             String[] categories = { NOUN, VERB, ADJECTIVE, ADVERB, "EXIT" };
-            int category = JOptionPane.showOptionDialog(f, "Specify a Category or click \"EXIT\" to stop.", "Let's expand the story!",
+            int category = JOptionPane.showOptionDialog(f, "Specify a Category or click \"EXIT\" to stop. \nYou can click a Category multiple times to add different values to it :)", "Let's expand the story!",
                     JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, categories, categories[0]);
             String categoryText = categories[category];
             if(categoryText.equals("EXIT")) {
@@ -111,5 +113,13 @@ public class Ex4_MadLib {
         f.dispose();
 
         return branchingStoryElements;
+    }
+
+    private static void printMainStory(Map<String, String> storyElements) {
+        System.out.println(MessageFormat.format("Do you {0} your {1} {2} {3}? That's hilarious!",
+                storyElements.get(VERB),
+                storyElements.get(ADJECTIVE),
+                storyElements.get(NOUN),
+                storyElements.get(ADVERB)));
     }
 }
