4c462d06013dcc5a05e1f6b713b51825172fde35
Alessia <volpi.alessia@hotmail.it>
2024-11-11 11:45:29 +0100

    Exercise 24 - Anagram Checker

---
+++ Chapter 5 - Functions/src/Ex25_AnagramChecker.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.lang.System.in;
+
+public class Ex25_AnagramChecker {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(in);
+
+        System.out.print("Enter two strings and I'll tell you if they are anagrams:\n" +
+                         "Enter the first string: ");
+        String firstString = sc.nextLine();
+        System.out.print("Enter the second string: ");
+        String secondString = sc.nextLine();
+
+        boolean isAnagram = isAnagram(firstString,secondString);
+
+        System.out.println("\"" + firstString + "\" and \"" + secondString + "\" are " +
+                           (isAnagram ? "" : "not ") + "anagrams");
+
+        sc.close();
+    }
+
+    private static boolean isAnagram(String firstString, String secondString) {
+        List<Character> secondStringList = new ArrayList<>();
+        for (char c : secondString.toCharArray()) {
+            secondStringList.add(c);
+        }
+
+        AtomicBoolean isAnagram = new AtomicBoolean(true);
+        for (char c : firstString.toCharArray()) {
+            if (!secondStringList.contains(c)) {
+                isAnagram.set(false);
+                break;
+            }
+        }
+
+        return firstString.length() == secondString.length() && isAnagram.get();
+    }
+}
