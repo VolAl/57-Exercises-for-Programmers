ff99b1e7527735aaed96fcb90b302b4492f45ae8
Alessia <volpi.alessia@hotmail.it>
2024-11-12 21:33:24 +0100

    Exercise 30 - Multiplication Table

---
+++ Chapter 6 - Repetition/src/Ex30/Ex30_MultiplicationTable.java
@@ -0,0 +1,14 @@
+package Ex30;
+
+public class Ex30_MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i<=12; i++) {
+            for(int j=0; j<=12; j++) {
+                System.out.println(i + " x " + j + " = " + (i*j));
+            }
+        }
+
+    }
+}
